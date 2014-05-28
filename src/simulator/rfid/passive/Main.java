858fce3dacc6f85f6eae1823e397255ca05f62a8
Rafael Perazzo <rafaelperazzo@gmail.com>
2014-05-28 10:42:11 -0300

    Version 1.0 28/05/2014 10:41

--- src/simulator/rfid/passive/Main.java
+++ src/simulator/rfid/passive/Main.java
@@ -22,7 +22,9 @@ public class Main {
 		
 		int begin=100,end=1000,steps=100,ci=90, iterations=100;
 		int method=1, deleteStats=1, initialFrameSize=128;
-		if (args.length==8) {
+		String all = "no";
+		
+		if (args.length==9) {
 			begin=Integer.parseInt(args[0]);
 			end=Integer.parseInt(args[1]);
 			steps=Integer.parseInt(args[2]);
@@ -31,13 +33,36 @@ public class Main {
 			method=Integer.parseInt(args[5]);
 			deleteStats=Integer.parseInt(args[6]);
 			initialFrameSize=Integer.parseInt(args[7]);
+			all = args[8];
 			if (deleteStats==1) {
 				File f = new File("stats.txt");
 				if (f.exists()) f.delete();
 			}
-			Thread sim = new Thread(new Simulator(begin,method, initialFrameSize,iterations,ci,end,steps,true));
-			sim.start(); 
+			if (all.equals("no")) {
+				Thread sim = new Thread(new Simulator(begin,method, initialFrameSize,iterations,ci,end,steps,true));
+				sim.start();
+			}
+			else {
+				Simulator schoute = new Simulator(begin,1, 128,iterations,ci,end,steps,true);
+				schoute.startDFSA();
+				System.gc();
+				Simulator lower = new Simulator(begin,2, 128,iterations,ci,end,steps,true);
+				lower.startDFSA();
+				System.gc();
+				Simulator eomlee = new Simulator(begin,3, 128,iterations,ci,end,steps,true);
+				eomlee.startDFSA();
+				System.gc();
+				Simulator mota = new Simulator(begin,4, 4,iterations,ci,end,steps,true);
+				mota.startDFSA();
+				System.gc();
+				Simulator c1g2 = new Simulator(begin,5, 4,iterations,ci,end,steps,true);
+				c1g2.startDFSA();
+				
+			}
+			 
 		}
+		
+				
 		else {
 			System.out.println("Usage: ");
 			System.out.println("java -jar <initialNumberOfTags> <finalNumberOfTags> <step> <confidenceLevel> <numberOfIterations> <method> <deleteStatusFile> <initialFrameSize>");
